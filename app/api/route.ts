b6e3226b7908153d92f3f53a38ddaa3c1608023d
francis mwaniki <francis@kali.franc>
2024-01-03 21:01:15 +0300

    bug fixing

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { AssemblyAI } from 'assemblyai';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+export const runtime = 'edge'; // 'nodejs' is the default
 export async function POST(req: Request, res: Response) {
      const apiKey = process.env.ASSEMBLY_API_KEY || '';
 
